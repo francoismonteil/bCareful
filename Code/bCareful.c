1ed008ec0563bcd131193808097dede6d09aab7d
francoismonteil <44313306+francoismonteil@users.noreply.github.com>
2019-06-16 16:05:50 +0200

    update bCareful.c
    Problème d'écriture du fichier json à cause du while

--- Code/bCareful.c
+++ Code/bCareful.c
@@ -1,25 +1,46 @@
 #include "temperature_humidite.c"
 #include "luminosite.c"
-//#include "mic.c"
+#include "mic.c"
 #include "json.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
 
 //Thread Micro
 
-int main()
+//Thread lecture capteurs
+void *lecture(void *arg)
 {
 	if(wiringPiSetup() == -1)
 		exit(0);
 
-	printf("Acquisition des donées\n");
-	char * temp = read_dht11_temp();
-	char * hum = read_dht11_hum();
-	float lum = luminosite();
+	while(1){
+		delay(10000);
+		printf("Acquisition des donées\n");
+		char * temp = read_dht11_temp();
+		char * hum = read_dht11_hum();
+		char * lum = luminosite();
 
-	printf("Temperature :%s °C \n", temp);
-	printf("Humidite :%s %c \n", hum, '%');
-	printf("Luminosite :%f %c \n", lum, '%');
+		printf("Temperature :%s °C \n", temp);
+		printf("Humidite :%s %c \n", hum, '%');
+		printf("Luminosite :%s %c \n", lum, '%');
 
-	json(temp, hum, lum);
-	//printf("%d\n", read_pmo_lum());
+		json(temp, hum, lum);
+	}
+	pthread_exit(NULL);
+}
+
+int main()
+{
+	pthread_t thread1;
+	
+	if(pthread_create(&thread1, NULL, lecture, NULL) == -1){
+		perror("pthread_create");
+		return EXIT_FAILURE;
+	}
+	
+	pthread_join(thread1, NULL);
+	
 	return 0;
 }
