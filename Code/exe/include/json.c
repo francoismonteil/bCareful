03b1ba156af7d9443dacbfc1c24fc8bbbb94efc7
francois-ubuntu <francois.monteil@ig2i.centralelille.fr>
2019-06-17 13:15:37 +0200

    Ecriture json OK

--- Code/exe/include/json.c
+++ Code/exe/include/json.c
@@ -1,6 +1,22 @@
 #include <string.h>
-#include <time.h> 
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int compteChar(char *dir){
+	FILE *fichier = fopen(dir, "r+");
+	int nChar = 0;
+	int c;
+	while((c=fgetc(fichier)) != EOF)
+	{
+		nChar++;
+		if(c==']'){
+			break;
+			printf("fin\n");
+		}
+	}
+	return nChar;
+}
 
 int json(char * temp, char * hum, char * lum, int pleure){
 	time_t timestamp = time(NULL);
@@ -9,23 +25,34 @@ int json(char * temp, char * hum, char * lum, int pleure){
 	FILE *fic;
 	
 	if(pleure == 1){
-		sprintf(dir, "/var/www/html/bCareful/data/pleure.json");
+		sprintf(dir, "pleure.json");
 		fic = fopen(dir, "w");
+	}else{
+		sprintf(dir, "data.json");
+		fic = fopen(dir, "r+");
+		int nbChars;
+		if((nbChars = compteChar(dir)) == 0){
+			fprintf(fic, "[\n");	
+				fprintf(fic, "\t{\n");
+					fprintf(fic, "\t\t\"Temperature\": %s,\n", temp);
+					fprintf(fic, "\t\t\"Humidite\": %s,\n", hum);
+					fprintf(fic, "\t\t\"Luminosite\": %s,\n", lum);
+					fprintf(fic, "\t\t\"Timestamp\": %ld\n", timestamp);
+				fprintf(fic, "\t}\n");
+			fprintf(fic, "]\n");
+		}else{
+			fseek(fic, nbChars-3, 0);
+					fprintf(fic, "},\n");
+					fprintf(fic, "\t{\n");
+					fprintf(fic, "\t\t\"Temperature\": %s,\n", temp);
+					fprintf(fic, "\t\t\"Humidite\": %s,\n", hum);
+					fprintf(fic, "\t\t\"Luminosite\": %s,\n", lum);
+					fprintf(fic, "\t\t\"Timestamp\": %ld\n", timestamp);
+				fprintf(fic, "\t}\n");
+			fprintf(fic, "]\n");
+		}
 	}
-	else{
-		sprintf(dir, "/var/www/html/bCareful/data/%ld.json", timestamp);
-		fic = fopen(dir, "a");
-	}
-		
-	fprintf(fic, "[\n");	
-		fprintf(fic, "\t{\n");
-			fprintf(fic, "\t\t\"Temperature\": %s,\n", temp);
-			fprintf(fic, "\t\t\"Humidite\": %s,\n", hum);
-			fprintf(fic, "\t\t\"Luminosite\": %s\n", lum);
-			fprintf(fic, "\t\t\"Timestamp\": %ld\n", timestamp);
-		fprintf(fic, "\t}\n");
-	fprintf(fic, "]\n");
-	
+			
 	fclose(fic);
 	
 	return 1;
