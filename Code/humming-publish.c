4382bc51114ba62d03a1d917d0484cfb12b2ceb5
alexbodart <43186738+alexbodart@users.noreply.github.com>
2019-06-23 22:50:52 +0200

    4.0 la bonne en code

--- Code/humming-publish.c
+++ Code/humming-publish.c
@@ -121,7 +121,7 @@ const uint8_t battery_status_flag_desc_err_index = 2;
 void iot_publish_variable(const char* name, int32_t value){
     sprintf(dir, "/home/pi/Downloads/bCareful-master/data/data.json");
     fic = fopen(dir, "a");
-    fprintf(fic, "\n\t{\n\t\t\"%s\" : %d\n\t}", name, value);
+    fprintf(fic, "\n\t{\n\t\t\"%s\" : %d\n\t},", name, value);
     fclose(fic);
 }
 
